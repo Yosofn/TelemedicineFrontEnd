f11408af1a16f3dce1a94e8b3f59a3f69abaa366
Yosofn <yousefnasr68@gmail.com>
2023-01-24 22:57:27 +0200

    main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoctordetailsComponent } from './doctordetails/doctordetails.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "user/login", pathMatch: "full" },
+  { path: 'register', component:RegisterComponent },
+  { path: 'home', component:HomeComponent },
+  { path: 'login', component:LoginComponent },
+  { path: 'doctors', component:DoctorsComponent },
+  { path: 'doctordetails', component:DoctordetailsComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { DoctordetailsComponent } from './doctordetails/doctordetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    DoctorsComponent,
+    DoctordetailsComponent
   ],
   imports: [
     BrowserModule,

e237ddcb0b8489ef1a0c57763cc20f14a00cc4c2
Yosofn <yousef>
2023-05-12 15:23:21 +0300

    connect to api 1

--- src/app/Auth/register/register.component.ts
+++ src/app/Auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Register } from '../interfaces/register';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  register:Register={
+  nationalId: 0,
+  username: "",
+  password: "",
+  age: 0,
+  address: "",
+  fname: "",
+  lname: "",
+  height: 0,
+  weight: 0,
+  job: "",
+  phone: "",
+  profileStatus: 1,
+  gender: "",
+  maritalStatus: ""
+  }
 
-  constructor() { }
+  maritalStatuses = ['Cardiologist', 'Neurologist', 'Pediatrician', 'Surgeon', 'Psychiatrist', 'Anesthesiologist', 'Internal Medicine Physician', 'Gynecologist'];
+
+  constructor(
+    private router:Router,
+    private AuthenticationService:AuthenticationService
+  ) { }
 
   ngOnInit(): void {
   }
 
-}
+  SumbitRegister(register:Register){
+
+
+  this.AuthenticationService.Register(register).subscribe((response)=>{
+  if (response.success){
+   this.router.navigate(['/login']);
+  
+  
+  }
+
+     } ) }    
+  }
+
+
